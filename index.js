11353204ded43bb2b70efeb6c441fc71c92100e7
mcampoynaranjo <mcampoynaranjo@alumnos.viu.es>
2022-04-26 22:04:44 +0200

    1 file modified

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var visualization = d3plus.viz()
 .y("value")
 .draw()
 
-
+//Gráfico de multicolumnas. Mi 2º gráfico
 d3.json("https://raw.githubusercontent.com/raul27868/07MBIG-Visualizacion-Actividades-Guiadas/master/columnas.json", function(data) {
   var visualization = d3plus.viz()
   .container("#viz2")
@@ -24,3 +24,31 @@ d3.json("https://raw.githubusercontent.com/raul27868/07MBIG-Visualizacion-Activi
   .axes({ ticks: 'false' })
   .draw();
   });
+
+//Gráfico de caja y bigotes. Mi 3º gráfico
+
+var visualization = d3plus.viz()
+.container("#viz3")
+.data([
+  {"year": 1991, "name":"alpha", "value": 15},
+  {"year": 1992, "name":"alpha", "value": 34},
+  {"year": 1991, "name":"alpha2", "value": 17},
+  {"year": 1992, "name":"alpha2", "value": 65},
+  {"year": 1991, "name":"beta", "value": 10},
+  {"year": 1992, "name":"beta", "value": 10},
+  {"year": 1991, "name":"beta2", "value": 40},
+  {"year": 1992, "name":"beta2", "value": 38},
+  {"year": 1991, "name":"gamma", "value": 5},
+  {"year": 1992, "name":"gamma", "value": 10},
+  {"year": 1991, "name":"gamma2", "value": 20},
+  {"year": 1992, "name":"gamma2", "value": 34},
+  {"year": 1991, "name":"delta", "value": 50},
+  {"year": 1992, "name":"delta", "value": 43},
+  {"year": 1991, "name":"delta2", "value": 17},
+  {"year": 1992, "name":"delta2", "value": 35}
+])
+.type("box")
+.id("name")
+.x("year")
+.y("value")
+.draw()
